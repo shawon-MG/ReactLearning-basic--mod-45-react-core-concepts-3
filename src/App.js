dfb7ac954a4b32d49c174b940bfc695ac8b7f1ef
shawon-MG <work.shawomg@gmail.com>
2022-03-20 23:26:28 +0600

    Load dynamic data, API call useEffect integrate state---are used

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+
+
+function ExternalUsers(){
+  
+  const [users, setUsers] = useState([]);   /* ----- Using useState():       */
+  
+  useEffect( () => {                       /* ------- Using useEffect() :   useEffect (() => { } , [])     */   
+  
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json()) 
+    .then(data => setUsers(data))  /* useEffect er moddhe data fetch kore ene setake setUsers e rakhlam. */
+
+  } , [])
+
+  return (
+    <div>
+      <h1>Loading API users from JSON Placeholder</h1>
+      {
+        users.map(user => <Users name={user.name} email={user.email}></Users>)  /* User er jonno banano component ke ei arrow func er moddhe return korlam. */
+      }
+    </div>
+  )
+}
+
+function Users(props){         /* User er jonno arekta component banalam. prpos soho.  */    
+  return (
+    <div style={{backgroundColor: 'lightblue', padding: '15px', margin: '15px', border: '3px solid black', borderRadius: '20px'}}>
+      <h4> Name: {props.name}</h4>
+      <h5> Email: {props.email}</h5>
+    </div>
+  )
 }
 
 export default App;
